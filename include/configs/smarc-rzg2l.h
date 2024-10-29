b8f6e1a314fc694d1591c9ea2f6e0599e64cd8e8
zhangyunduan <zhangyunduan@linyang.com.cn>
2024-10-29 09:02:16 +0800

    add lcd support

--- include/configs/smarc-rzg2l.h
+++ include/configs/smarc-rzg2l.h
@@ -65,9 +65,10 @@
 	"prodsdbootargs=setenv bootargs rw rootwait earlycon root=/dev/mmcblk1p2 \0" \
 	"prodemmcbootargs=setenv bootargs rw rootwait earlycon root=/dev/mmcblk0p2 \0" \
 	"bootimage=unzip 0x4A080000 0x48080000; booti 0x48080000 - 0x48000000 \0" \
-	"emmcload=ext4load mmc 0:2 0x48080000 boot/Image;ext4load mmc 0:2 0x48000000 boot/r9a07g044l2-smarc.dtb;run prodemmcbootargs \0" \
+	"emmcload=fatload mmc 0:1 0x48080000 Image;fatload mmc 0:1 0x48000000 ly-rzg2l-smarc.dtb;run prodemmcbootargs \0" \
 	"sd1load=ext4load mmc 1:2 0x48080000 boot/Image;ext4load mmc 1:2 0x48000000 boot/r9a07g044l2-smarc.dtb;run prodsdbootargs \0" \
-	"bootcmd_check=if mmc dev 1; then run sd1load; else run emmcload; fi \0"
+	"bootcmd_check=if mmc dev 1; then run sd1load; else run emmcload; fi \0" \
+	"tftpload=tftp 0x48080000 Image;tftp 0x48000000 ly-rzg2l-smarc.dtb;tftp 0x50000000 RZG2L-LY.cpio.gz.u-boot;booti 0x48080000 0x50000000 0x48000000 \0"
 
 #define CONFIG_BOOTCOMMAND	"env default -a;run bootcmd_check;run bootimage"
 
